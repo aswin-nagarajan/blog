2091dfeff4029d3cc8c3679d9490ab3a38ebbd62
aswin-nagarajan <aswin97.nagarajan@gmail.com>
2017-06-07 13:48:12 -0400

    CRUD articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,26 @@
 class ArticlesController < ApplicationController
+ 
+  def index
+    @articles = Article.all
+  
+  
+  end
+  
+  
   def new
     @article = Article.new
   
+  end
+  
+  def edit
+    @article = Article.find(params[:id])  
   end
   
   def create
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created"
-      redirect_to article_path(@article)
+      redirect_to articles_path
     
     else
       render :new
@@ -16,11 +28,28 @@ class ArticlesController < ApplicationController
     
   end
   
+  def update 
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to articles_path
+    else
+      render :edit
+    end
+    
+  end
   def show
     @article = Article.find(params[:id])
   end
   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice]="Article was successfully deleted"
+    redirect_to articles_path
+    
   
+  end
   
   private
   def article_params
